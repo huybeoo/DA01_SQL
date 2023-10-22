930fad5e5fb6bab1a21b93a74dd913304e294ea3
huybeoo <147389087+huybeoo@users.noreply.github.com>
2023-10-22 21:27:13 +0700

    Update practice5.sql

--- practice5.sql
+++ practice5.sql
@@ -8,7 +8,7 @@ ROUND(SUM(CASE WHEN t.signup_action = 'Confirmed' THEN 1 ELSE 0 END)*1.0 / COUNT
 FROM emails e LEFT JOIN texts t  
 ON e.email_id  = t.email_id
 --ex3
-
+em chua hieu cau nay
 --ex4
 SELECT C.customer_id FROM customer_contracts C , products P
 where C.product_id = P.product_id
