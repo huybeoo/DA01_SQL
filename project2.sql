fc3e4606523fa087a6bd30bcccc3b8321f1c8afc
huybeoo <147389087+huybeoo@users.noreply.github.com>
2023-11-11 18:03:09 +0700

    Update project2.sql

--- project2.sql
+++ project2.sql
@@ -42,3 +42,21 @@ sum(t.sale_price)
 FROM bigquery-public-data.thelook_ecommerce.products s
 join bigquery-public-data.thelook_ecommerce.order_items t on s.id = t.product_id
 group by 1,2
+
+
+select EXTRACT(YEAR FROM ord.created_at) || '-' || EXTRACT(MONTH FROM ord.created_at)|| '-' || '01' as year,
+extract (month from ord.created_at) as month,
+sum(ordi.sale_price) as tpv,
+count(ordi.order_id) as tpo,
+round(100 * (sum(ordi.sale_price) - lag(sum(ordi.sale_price)) over(partition by 1,2 order by 1,2 ))/lag(sum(ordi.sale_price)) over(partition by 1,2 order by 1,2 ),2)||'%' as Revenue_growth,
+round(100 * (count(ordi.order_id) -lag(count(ordi.order_id)) over(partition by 1,2 order by 1,2))/lag(count(ordi.order_id)) over(partition by 1,2 order by 1,2),2)||'%' as Order_growth,
+sum(pd.cost) as total_cost,
+sum(ordi.sale_price) - sum(pd.cost) as total_profit,
+sum(ordi.sale_price) / sum(pd.cost) as Profit_to_cost_ratio
+from bigquery-public-data.thelook_ecommerce.orders ord
+join bigquery-public-data.thelook_ecommerce.order_items ordi
+on ord.order_id = ordi.order_id
+join bigquery-public-data.thelook_ecommerce.products pd
+on ordi.product_id = pd.id
+group by 1,2
+
